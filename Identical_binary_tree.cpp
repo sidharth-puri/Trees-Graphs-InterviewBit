2bd21297acf969518910ebff2743933aa00381c4
sidharth-puri <52946626+sidharth-puri@users.noreply.github.com>
2020-01-26 13:41:31 +0530

    Create Identical_binary_tree.cpp

---
+++ Identical_binary_tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int Solution::isSameTree(TreeNode* A, TreeNode* B) {
+    if(A==NULL && B==NULL){
+        return 1;
+    }
+    if(A!=NULL && B==NULL){
+        return 0;
+    }
+    if(A==NULL && B!=NULL){
+        return 0;
+    }
+    if(A->val==B->val && isSameTree(A->left,B->left)&& isSameTree(A->right,B->right)){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
