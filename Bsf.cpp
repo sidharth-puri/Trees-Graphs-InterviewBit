8ec0c567875ca2d8b7a7bd1be1a81781ba676645
Sidharth Puri <52946626+sidharth-puri@users.noreply.github.com>
2020-05-17 18:38:16 +0530

    Create Bsf.cpp

---
+++ Bsf.cpp
@@ -0,0 +1,19 @@
+void bfs(int s, vector<int> g[], vector<bool> &vis, int N) {
+
+    queue<int> q;
+    q.push(s);
+    vis[s]=true;
+    while(!q.empty()) {
+    s=q.front();
+    cout<<q.front()<<" ";
+    q.pop();
+    vector<int>:: iterator i;
+  
+    for(i=g[s].begin();i<g[s].end();i++) {
+        if(!vis[*i]) {
+              vis[*i]=true;
+              q.push(*i);
+        }
+    }
+    }
+}
