26c09c48ed47457827bd959c277cb4cf445b1e1f
Sidharth Puri <52946626+sidharth-puri@users.noreply.github.com>
2020-05-21 01:11:05 +0530

    Update LCA_bst.cpp

--- LCA_bst.cpp
+++ LCA_bst.cpp
@@ -4,20 +4,20 @@ struct Node {
     Node* left;
 
 };
-*/
+
 
 /*You are required to complete
 this function*/
 Node* LCA(Node *root, int n1, int n2)
 {
-   //Your code here
+
    if(root==NULL)
    {
        return root;
    }
    while(root!=NULL)
    {
-       // if both are less than root then lca lies in left subtree as soon as we encounter a root having one of the values grater and other lesser than root
+      
        if(n1 < root->data && n2 < root->data)
        {
            root = root->left;
