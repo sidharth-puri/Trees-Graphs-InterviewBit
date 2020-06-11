6d40c3cab26cbfa555ba7f39b9131a43f0a1c021
Sidharth Puri <52946626+sidharth-puri@users.noreply.github.com>
2020-06-11 12:38:29 +0530

    Update Next_greatest_number_bst.c

--- Next_greatest_number_bst.c
+++ Next_greatest_number_bst.c
@@ -1,27 +1,4 @@
-/**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- * 
- * typedef struct TreeNode treenode;
- * 
- * treenode* treenode_new(int val) {
- *     treenode* node = (treenode *) malloc(sizeof(treenode));
- *     node->val = val;
- *     node->left = NULL;
- *     node->right = NULL;
- *     return node;
- * }
- */
-/**
- * @input A : Root pointer of the tree 
- * @input B : Integer
- * 
- * @Output root pointer of tree.
- */
+
 treenode* getSuccessor(treenode* A, int B) {
     treenode* prev=NULL;
     treenode* current=A;
